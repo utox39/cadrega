247f61dded2b29f0b18009c41cd9e9a8bcdcf637
utox39 <francescomoccaldi39@gmail.com>
2026-04-28 12:09:51 +0200

    feat(parser): improve the url detection

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -8,15 +8,20 @@ import (
 )
 
 var (
-	urlRegex = regexp.MustCompile(`https?://[^\s<>"{}|\\^` + "`" + `\[\]]+`)
-	shRegex  = regexp.MustCompile(`curl .* \| (bash|sh|source)|wget .* -O- \| bash|npx [a-z0-9-]+|pip install .*`)
+	urlRegex            = regexp.MustCompile(`https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)`)
+	trailingNonAlphaNum = regexp.MustCompile(`[^a-zA-Z0-9]+$`) // utility regex
+	shRegex             = regexp.MustCompile(`curl .* \| (bash|sh|source)|wget .* -O- \| bash|npx [a-z0-9-]+|pip install .*`)
 )
 
 // GetURLs extracts all HTTP and HTTPS URLs from data.
 //
 // Returns the matched URLs, or nil if none are found.
 func GetURLs(data string) []string {
-	return urlRegex.FindAllString(data, -1)
+	matches := urlRegex.FindAllString(data, -1)
+	for i, m := range matches {
+		matches[i] = trailingNonAlphaNum.ReplaceAllString(m, "")
+	}
+	return matches
 }
 
 // GetShellCommands extracts potentially dangerous shell command patterns from data.
